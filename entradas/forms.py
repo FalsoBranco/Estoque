c04bb67d9ed78b06cf41993d5c35cb64ad6e7eea
helter daniel <helterdaniel@hotmail.com>
2021-05-21 16:03:31 -0300

    widget entrada

--- entradas/forms.py
+++ entradas/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import widgets
 
 from .models import Entrada
 
@@ -7,3 +8,16 @@ class EntradaForm(forms.ModelForm):
     class Meta:
         model = Entrada
         fields = ["produto", "preco", "quantidade"]
+
+        widgets = {
+            "produto": widgets.Select(),
+            "preco": widgets.NumberInput(
+                attrs={
+                    "class": "input",
+                    "min": "0",
+                    "placeholder": "Preço do produto",
+                    "step": "0.01",
+                }
+            ),
+            "quantidade": widgets.NumberInput(attrs={"class": "input"}),
+        }
