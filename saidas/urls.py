7d669b0173137e00fe032d2bea5b20549e814421
helter daniel <helterdaniel@hotmail.com>
2021-06-08 19:59:34 -0300

    form dinamico com ajax e update saida

--- saidas/urls.py
+++ saidas/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", views.SaidaView.as_view(), name="index"),
     path("lista/", views.SaidaListView.as_view(), name="lista_saidas"),
     path("nova/", views.SaidaCreateView.as_view(), name="nova_saida"),
-    # path("lista/", views.SaidaListView.as_view(), name="lista_saidas"),
-    # path("lista/", views.SaidaListView.as_view(), name="lista_saidas"),
+    path("delete/<int:pk>", views.SaidaDeleteView.as_view(), name="deleta_saida"),
+    path("alterar/<int:pk>", views.SaidaUpdateView.as_view(), name="alterar_saida"),
+    path("get-produto-ajax/", views.get_produto_ajax, name="get_produto_ajax"),
 ]
--- saidas/views.py
+++ saidas/views.py
@@ -6,8 +6,9 @@ from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
+from produtos.models import Produto
 
 from saidas.forms import SaidaForm
 from saidas.models import Saida
@@ -37,3 +38,47 @@ class SaidaCreateView(CreateView):
         form.instance.preco = form.instance.produto.preco
         form.instance.produto.save()
         return super().form_valid(form)
+
+
+# ! TODO Procurar formas melhores de fazer esse update no foreing key
+class SaidaDeleteView(DeleteView):
+    model = Saida
+    success_url = reverse_lazy("saidas:lista_saidas")
+
+    def get(self, *args, **kwargs):
+        saida = Saida.objects.get(pk=self.kwargs.get("pk"))
+        saida.produto.quantidade += saida.quantidade
+        saida.produto.save_base()
+        return self.post(*args, **kwargs)
+
+
+class SaidaUpdateView(UpdateView):
+    model = Saida
+    form_class = SaidaForm
+    template_name = "saidas/saida_update.html"
+    success_url = reverse_lazy("saidas:lista_saidas")
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+
+        form.instance.produto.quantidade = (
+            form.instance.produto.quantidade - form.initial.get("quantidade")
+        ) + form.instance.quantidade
+        form.instance.produto.save_base()
+        return super().form_valid(form)
+
+
+from django.http import JsonResponse
+
+
+def get_produto_ajax(request):
+    data = {}
+    if request.method == "POST":
+        produto_id = request.POST["produto_id"]
+
+        try:
+            produto = Produto.objects.filter(id=produto_id)
+
+        except Exception:
+            data["error_message"] = "error"
+            return JsonResponse(data)
+        return JsonResponse(list(produto.values("id", "quantidade")), safe=False)
