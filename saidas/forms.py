0d4c7948cf38d1420e3becb7dc3ce219ecddcfd9
helter daniel <helterdaniel@hotmail.com>
2021-06-08 19:57:39 -0300

    Saida widget

--- saidas/forms.py
+++ saidas/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import widgets
 
 from .models import Saida
 
@@ -7,3 +8,8 @@ class SaidaForm(forms.ModelForm):
     class Meta:
         model = Saida
         fields = ("produto", "quantidade")
+
+        widgets = {
+            "produto": widgets.Select(),
+            "quantidade": widgets.NumberInput(attrs={"class": "input", "min": "0"}),
+        }
